be1161070b9a2a3f42a38f9cb2535eafc0e1bcb4
Lena <lhaefele@gmail.com>
2023-07-02 20:59:07 -0400

    my attempt

--- social_network_classes.py
+++ social_network_classes.py
@@ -1,4 +1,5 @@
 # A class to hold general system wide social media data and functions. Eg Data objects of all people, Eg functions: Save social media to disk
+import json
 class SocialNetwork:
     def __init__(self):
         self.list_of_people = []
@@ -7,6 +8,9 @@ class SocialNetwork:
         
     ## For more challenge try this
     def save_social_media(self):
+        json.dumps(SocialNetwork)
+        json.dumps(Person)
+
         # function to save social media to a file on disk 
         # hint: look up how to use python's inbuil json module to turn objects to json
         # you can write this json unto a file on disk
